99026c0d973c0ba46a3087356a0987813dc664a5
Ganeshk750 <ganesh9ot@gmail.com>
2020-06-24 11:22:18 +0530

    initial commit

---
+++ Spring-boot-with-email/src/main/java/com/ganesh/api/SpringBootWithEmailApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithEmailApplication.class, args);
+	}
+
+}
---
+++ Spring-boot-with-email/src/test/java/com/ganesh/api/SpringBootWithEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganesh.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
